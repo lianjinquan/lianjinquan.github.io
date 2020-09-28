79430c2eee680b5673a73a38d67dd0f82300218b
lianjinquan <1032905347@qq.com>
2020-09-28 22:15:27 +0800

    Site updated: 2020-09-28 22:15:24

---
+++ js/less.js
@@ -0,0 +1,160 @@
+/**UP酱*/
+/*添加图片top*/
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
+
+
+/* 手机客户端导航栏默认隐藏 */
+// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+// for (var i = 0; i < menus_item_child.length; i++) {
+//     menus_item_child[i].style.display = "none";
+//     menus_expand[i].className += " closed";
+// }
+
+// 可爱的Title
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(つェ⊂) 我藏好了哦~~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.title = '(*´∇｀*) 被你发现啦~~' + OriginTitle;
+        titleTime = setTimeout(function() {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
+
+// 气泡
+function qipao() {
+    $('#page-header').circleMagic({
+        radius: 10,
+        density: .2,
+        color: 'rgba(255,255,255,.4)',
+        clearOffset: 0.99
+    });
+}! function(p) {
+    p.fn.circleMagic = function(t) {
+        var o, a, n, r, e = !0,
+            i = [],
+            d = p.extend({ color: "rgba(255,0,0,.5)", radius: 10, density: .3, clearOffset: .2 }, t),
+            l = this[0];
+
+        function c() { e = !(document.body.scrollTop > a) }
+
+        function s() { o = l.clientWidth, a = l.clientHeight, l.height = a + "px", n.width = o, n.height = a }
+
+        function h() {
+            if (e)
+                for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
+            requestAnimationFrame(h)
+        }
+
+        function f() {
+            var t = this;
+
+            function e() { t.pos.x = Math.random() * o, t.pos.y = a + 100 * Math.random(), t.alpha = .1 + Math.random() * d.clearOffset, t.scale = .1 + .3 * Math.random(), t.speed = Math.random(), "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
+            t.pos = {}, e(), this.draw = function() { t.alpha <= 0 && e(), t.pos.y -= t.speed, t.alpha -= 5e-4, r.beginPath(), r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1), r.fillStyle = t.color, r.fill(), r.closePath() }
+        }! function() {
+            o = l.offsetWidth, a = l.offsetHeight,
+                function() {
+                    var t = document.createElement("canvas");
+                    t.id = "canvas", t.style.top = 0, t.style.zIndex = 0, t.style.position = "absolute", l.appendChild(t), t.parentElement.style.overflow = "hidden"
+                }(), (n = document.getElementById("canvas")).width = o, n.height = a, r = n.getContext("2d");
+            for (var t = 0; t < o * d.density; t++) {
+                var e = new f;
+                i.push(e)
+            }
+            h()
+        }(), window.addEventListener("scroll", c, !1), window.addEventListener("resize", s, !1)
+    }
+}(jQuery);
+
+// 调用气泡方法
+qipao();
+
+
+
+
+//页脚博主
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 连金全');
+});
+
+
+// 移除页脚样式-页脚全透明
+var app = document.getElementById('footer');
+app.removeAttribute('id')
+
+
+// 注意：live2d_path 参数应使用绝对路径
+const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+//const live2d_path = "/live2d-widget/";
+
+
+
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+    return new Promise((resolve, reject) => {
+        let tag;
+
+        if (type === "css") {
+            tag = document.createElement("link");
+            tag.rel = "stylesheet";
+            tag.href = url;
+        }
+        else if (type === "js") {
+            tag = document.createElement("script");
+            tag.src = url;
+        }
+        if (tag) {
+            tag.onload = () => resolve(url);
+            tag.onerror = () => reject(url);
+            document.head.appendChild(tag);
+        }
+    });
+}
+
+// 加载 waifu.css live2d.min.js waifu-tips.js
+if (screen.width >= 768) {
+    Promise.all([
+        loadExternalResource(live2d_path + "waifu.css", "css"),
+        loadExternalResource(live2d_path + "live2d.min.js", "js"),
+        loadExternalResource(live2d_path + "waifu-tips.js", "js")
+    ]).then(() => {
+        initWidget({
+            waifuPath: live2d_path + "waifu-tips.json",
+            //apiPath: "https://live2d.fghrsh.net/api/",
+            cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+        });
+    });
+}
+// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
+// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
+// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
+
+console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
