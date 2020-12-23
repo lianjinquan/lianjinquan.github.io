b89f956d8f484b1edbc952155de33256bca619e1
lianjinquan <1032905347@qq.com>
2020-12-23 21:23:47 +0800

    Site updated: 2020-12-23 21:23:36

--- js/style.js
+++ js/style.js
@@ -1,19 +1,3 @@
-/**UP酱*/
-/*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-/*添加到返回顶部按钮下*/
-document.getElementById("go-up").innerHTML += top_up;
-
-
-/* 手机客户端导航栏默认隐藏 */
-// var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-// var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-// var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-// for (var i = 0; i < menus_item_child.length; i++) {
-//     menus_item_child[i].style.display = "none";
-//     menus_expand[i].className += " closed";
-// }
-
 // 可爱的Title
 var OriginTitle = document.title;
 var titleTime;
@@ -139,3 +123,22 @@ if (screen.width >= 768) {
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
 // 初始化看板娘会自动加载指定目录下的 waifu-tips.json
 
+console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
